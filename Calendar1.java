0dfcf9a3872bc7c9c0037a29cc810eff5333e985
ophereevan17 <ophere.evan@post.runi.ac.il>
2024-01-03 18:04:58 +0200

    HW_03
    try2

--- Calendar1.java
+++ Calendar1.java
@@ -20,15 +20,15 @@ public class Calendar1 {
 	    int debugDaysCounter = 0; 
 	    //// Write the necessary initialization code, and replace the condition
 	    //// of the while loop with the necessary condition 
-	 	while (true) {
+	 	//while (true) {
 	 		//// Write the body of the while 		
-	 		advance();
-	 		debugDaysCounter++;
+	 		//advance();
+	 		//debugDaysCounter++;
 	 		//// If you want to stop the loop after n days, replace the condition of the
 	 		//// if statement with the condition (debugDaysCounter == n)
-	 		if (false) { 
-	 			break;
-	 		}
+	 		//if (false) { 
+	 			//break;
+	 		//}
         }
 	 	//// Write the necessary ending code here
 	 }
